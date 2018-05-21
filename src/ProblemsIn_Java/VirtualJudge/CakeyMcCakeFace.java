361768a3dba90c580bb1357fb3f4b3c809ae8ebf
David Yepes Buitrago <(davidyepesb@gmail.com)>
2018-05-21 15:16:08 -0500

    Quadtrees

--- src/ProblemsIn_Java/VirtualJudge/CakeyMcCakeFace.java
+++ src/ProblemsIn_Java/VirtualJudge/CakeyMcCakeFace.java
@@ -19,44 +19,47 @@ public class CakeyMcCakeFace {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(System.out);
 
-        StringTokenizer st;
-        long[] A, B;
-        int n, m;
-        String line;
-        while (true) {
-            line = in.readLine();
-            if (line == null)
-                break;
-            n = Integer.parseInt(line.trim());
-            m = Integer.parseInt(in.readLine().trim());
-            A = new long[n];
-            B = new long[m];
-            st = new StringTokenizer(in.readLine());
-            for (int i = 0; i < n; A[i] = Integer.parseInt(st.nextToken()), i++) ;
-            st = new StringTokenizer(in.readLine());
-            for (int i = 0; i < m; B[i] = Integer.parseInt(st.nextToken()), i++) ;
-            HashMap<Long, Long> R = new HashMap<>();
-            for (int i = 0, ii = 0; i < n; i++) {
-                while (ii < m && B[ii] <= A[i])
-                    ii++;
-                long v = A[i];
-                for (int j = ii; j < m; j++) {
-                    if (R.containsKey(B[j] - v))
-                        R.put(B[j] - v, R.get(B[j] - v) + 1);
-                    else
-                        R.put(B[j] - v, (long) 1);
-                }
-            }
-            long mx = 0, ret = 0;
-            for (Entry<Long, Long> e : R.entrySet()) {
-                if (e.getValue() > mx) {
-                    mx = e.getValue();
-                    ret = 32767;
-                }
-                if (e.getValue() == mx)
-                    ret = Math.min(ret, e.getKey());
-            }
-            out.println(ret);
-        }
-    }
+		StringTokenizer st;
+		long[] A, B;
+		int n, m;
+		String line;
+		while (true) {
+			line = in.readLine();
+			if (line == null)
+				break;
+			n = Integer.parseInt(line.trim());
+			m = Integer.parseInt(in.readLine().trim());
+			A = new long[n];
+			B = new long[m];
+			st = new StringTokenizer(in.readLine());
+			for (int i = 0; i < n; A[i] = Integer.parseInt(st.nextToken()), i++);
+			st = new StringTokenizer(in.readLine());
+			for (int i = 0; i < m; B[i] = Integer.parseInt(st.nextToken()), i++);
+			HashMap<Long, Long> R = new HashMap<>();
+			for (int i = 0, ii = 0; i < n; i++) {
+				while (ii < m && B[ii] <= A[i])
+					ii++;
+				long v = A[i];
+				for (int j = ii; j < m; j++) {
+					if (R.containsKey(B[j] - v))
+						R.put(B[j] - v, R.get(B[j] - v) + 1);
+					else
+						R.put(B[j] - v, (long) 1);
+				}
+			}
+			long mx = 0, ret = 0;
+			for (Entry<Long, Long> e : R.entrySet()) {
+				if (e.getValue() > mx) {
+					mx = e.getValue();
+					ret = 32767;
+				}
+				if (e.getValue() == mx)
+					ret = Math.min(ret, e.getKey());
+			}
+			out.println(ret);
+		}
+
+		in.close();
+		out.close();
+	}
 }

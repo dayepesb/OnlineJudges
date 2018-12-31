eacaf43f961aed9de4bfb61614dbf9b505f56f89
David Yepes Buitrago <(davidyepesb@gmail.com)>
2018-12-31 11:22:12 -0500

    VJudge Comparing Answers AC

---
+++ src/ProblemsIn_Java/VirtualJudge/IngeniousMetro.java
@@ -0,0 +1,17 @@
+package ProblemsIn_Java.VirtualJudge;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class IngeniousMetro {
+    public static void main(String [] args)throws Exception{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+
+
+        out.close();
+        in.close();
+    }
+}
